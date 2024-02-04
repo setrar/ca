fb3882ac15b939ed273976882681f8e0ee976587
setrar <setrar@valiha.com>
2024-02-04 20:52:27 +0100

    .

--- labs/2-syscall-io/io.s
+++ labs/2-syscall-io/io.s
@@ -8,6 +8,8 @@ print_char_message:
 .asciz "\nThe character you entered is: "
 bye_message:
 .asciz "\nBye!"
+print_ascii_message:
+.asciz "\nThe ASCII code of the character you entered is: "
 
 # code section
 .text
@@ -40,6 +42,19 @@ putc:
     addi  sp,sp,4     # deallocate stack frame, restore stack pointer
     ret               # return to caller
 
+# function: puti
+# description: print integer in a0 to console
+# parameters: integer to print in a0
+# return: none
+puti:
+    addi  sp, sp, -4    # Allocate stack frame
+    sw    ra, 0(sp)     # Save ra on stack
+    li    a7, 1         # Syscall code for PrintInt
+    ecall               # Call syscall
+    lw    ra, 0(sp)     # Restore ra from stack
+    addi  sp, sp, 4     # Deallocate stack frame
+    ret                 # Return to caller
+
 # function: main
 # description: read character from keyboard, print it to console, goto end if
 #              character is 'Q', else restart from beginning
@@ -58,6 +73,11 @@ main:
     ecall             # call syscall
     mv    a0,s0       # copy read character in a0
     call  putc        # call putc function to print character
+    la    a0, print_ascii_message
+    li    a7, 4
+    ecall
+    mv    a0, s0        # Move character ASCII code back into a0
+    call  puti          # Print ASCII code of the character
     li    t0,'Q'      # store 'Q' ASCII code in t0
     bne   a0,t0,main  # loop if read character is not 'Q'
     la    a0,bye_message # store address of message in a0
